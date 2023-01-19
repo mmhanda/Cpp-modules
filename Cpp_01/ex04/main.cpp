13c85cdd30f3380ca0556bcd6c0219110bb84cd0
mohammed <mhanda@e2r10p1.1337.ma>
2023-01-19 03:02:38 +0100

    Cpp01 ex04 done

--- Cpp_01/ex04/main.cpp
+++ Cpp_01/ex04/main.cpp
@@ -6,13 +6,71 @@
 /*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 12:26:46 by mhanda            #+#    #+#             */
-/*   Updated: 2023/01/18 12:27:55 by mhanda           ###   ########.fr       */
+/*   Updated: 2023/01/19 02:45:48 by mhanda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int ac , char **av)
 {
-    
+	if (ac != 4)
+	{
+		std::cout << "Error: Wrong number of arguments" << '\n';
+		return (1);
+	}
+	std::ifstream input_file(av[1]);
+	if (!input_file.good())
+	{
+		std::cout << "Error: input File not good" << '\n';
+		return (1);
+	}
+	std::ofstream output_file(std::string(av[1]) + ".replace");
+	if (!output_file.good())
+	{
+		std::cout << "Error: output File not good" << '\n';
+		input_file.close();
+		return (1);
+	}
+	std::string s1(av[2]);
+	std::string s2 = av[3];
+	std::string line;
+
+    char currentchar;
+    unsigned long count = 0;
+    while (input_file.get(currentchar)) {
+        if (currentchar == s1[count])
+		{
+            count++;
+            if (count == s1.length()) {
+                output_file << s2;
+                count = 0;
+            }
+        }
+		else
+		{
+            if (count != 0) {
+                for (unsigned long i = 0; i < count; i++) {
+                    output_file << s1[i];
+                }
+            }
+            output_file << currentchar;
+            count = 0;
+        }
+    }
+
+	input_file.close();
+	output_file.close();
 }
+
+
+
+
+
+
+
+
+
+
+
