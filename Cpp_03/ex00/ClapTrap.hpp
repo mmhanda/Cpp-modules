7697a221494135cc4326a5617a86379c10a42e88
mohammed <mhanda@e2r2p5.1337.ma>
2023-01-21 23:22:05 +0100

    updates on ex00

--- Cpp_03/ex00/ClapTrap.hpp
+++ Cpp_03/ex00/ClapTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 23:03:09 by mhanda            #+#    #+#             */
-/*   Updated: 2023/01/21 23:04:18 by mhanda           ###   ########.fr       */
+/*   Updated: 2023/01/21 23:19:06 by mhanda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,17 @@
 
 #include <iostream>
 
-
+class ClapTrap
+{
+    private:
+        std::string name;
+        int hit_points;
+        int enegy_points;
+        int attack_damage;
+    public :
+        void attack(const std::string& target);
+        void takeDamage(unsigned int amount);
+        void beRepaired(unsigned int amount);
+}
 
 #endif
