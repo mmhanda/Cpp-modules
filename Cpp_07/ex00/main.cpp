a8e67e52a6c7c3989e7d0c1432bca4278d9b3bf9
mohammed <mhanda@e2r2p9.1337.ma>
2023-02-07 23:31:28 +0100

    Cpp_07 ex00 done

--- Cpp_07/ex00/main.cpp
+++ Cpp_07/ex00/main.cpp
@@ -6,14 +6,52 @@
 /*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 02:18:44 by mhanda            #+#    #+#             */
-/*   Updated: 2023/02/06 02:20:19 by mhanda           ###   ########.fr       */
+/*   Updated: 2023/02/07 23:30:37 by mhanda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
+template <typename T>
+void swap(T &data, T&data1)
+{
+    T temp;
+    temp = data;
+    data = data1;
+    data1 = temp;
+}
+
+template <typename T>
+T min(T &data, T&data1)
+{
+    if (data == data1){
+        return (data);}
+    if (data < data1)
+        return (data);
+    else
+        return (data1);
+}
+
+template <typename T>
+T max(T &data, T&data1)
+{
+    if (data == data1){
+        return (data);}
+    if (data > data1)
+        return (data);
+    else
+        return (data1);
+}
+
 int main()
 {
-    
+    int a = 3;
+    int s = 9;
+
+    std::cout << "min " << min<int>(a, s) << std::endl;
+    std::cout << "max " << max<int>(a, s) << std::endl;
+    std::cout << a << " _ " << s << std::endl;
+    swap<int>(a, s);
+    std::cout << a  << " _ "  << s << std::endl;
     return (0);
 }
--- Cpp_07/ex00/whatever.hpp
+++ Cpp_07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 02:19:02 by mhanda            #+#    #+#             */
-/*   Updated: 2023/02/06 02:46:16 by mhanda           ###   ########.fr       */
+/*   Updated: 2023/02/07 23:29:45 by mhanda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,12 @@
 #include <iostream>
 
 template <typename T>
+void swap(T &data, T&data1);
 
+template <typename T>
+T max(T &data, T&data1);
 
+template <typename T>
+T min(T &data, T&data1);
 
 #endif
