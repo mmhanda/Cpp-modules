addab7d7fcc4137ff55c1c5a190d83befc2600bd
mohammed <mhanda@e2r2p6.1337.ma>
2023-02-17 06:26:09 +0100

    updates

--- Cpp_08/ex02/MutantStack.hpp
+++ Cpp_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 04:49:15 by mhanda            #+#    #+#             */
-/*   Updated: 2023/02/15 04:51:24 by mhanda           ###   ########.fr       */
+/*   Updated: 2023/02/15 10:00:41 by mhanda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,21 @@
 #include <stack>
 #include <list>
 
+
+// template <typename T>
+// class MutantStack : public std::stack<T>
+// {
+//     public:
+//         MutantStack(void);
+//         MutantStack(MutantStack const & src);
+//         ~MutantStack(void);
+
+//         MutantStack & operator=(MutantStack const & rhs);
+
+//         typedef typename std::stack<T>::container_type::iterator iterator;
+
+//         iterator begin(void);
+//         iterator end(void);
+// };
 
 #endif
--- Cpp_08/ex02/main.cpp
+++ Cpp_08/ex02/main.cpp
@@ -6,11 +6,35 @@
 /*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 04:48:42 by mhanda            #+#    #+#             */
-/*   Updated: 2023/02/15 04:53:10 by mhanda           ###   ########.fr       */
+/*   Updated: 2023/02/15 10:00:18 by mhanda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+    public:
+        MutantStack() {}
+        MutantStack(MutantStack const &src) : std::stack<T>(src) {}
+        virtual ~MutantStack() {}
+
+        MutantStack &operator=(MutantStack const &rhs)
+        {
+            if (this != &rhs)
+                std::stack<T>::operator=(rhs);
+            return *this;
+        }
+
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+        iterator begin() { return std::stack<T>::c.begin(); }
+        iterator end() { return std::stack<T>::c.end(); }
+        const_iterator begin() const { return std::stack<T>::c.begin(); }
+        const_iterator end() const { return std::stack<T>::c.end(); }
+};
 
 int main()
 {
