764ec8d8478378c1dc1a70d56c24628eb79a45d7
Mohammed Handa <mhanda@student.1337.ma>
2023-04-06 08:57:34 +0000

    fiel stack Cpp_09 ex02

--- Cpp_09/ex02/main.cpp
+++ Cpp_09/ex02/main.cpp
@@ -2,5 +2,37 @@
 
 int main(int ac, char **av) {
 
+    if (ac < 3) {
+        std::cout << "No enought argument" << std::endl;
+        return (1);
+    }
+    std::vector<int> numbers_v;
+    std::deque<int> numbers_d;
+    int i = 1;
+
+    try
+    {
+        while(av[i]) {
+
+            if (std::stoi(av[i]) > 0) {
+                numbers.push_back(std::stoi(av[i]));
+            }
+            else
+                throw std::runtime_error("check your argument");
+            i++;
+        }
+        ///////////////////       befor       /////////////////////////
+            std::cout << "Before:  ";
+            for (size_t i = 0; i < numbers.size(); i++)
+                std::cout << numbers.at(i) << " ";
+            std::cout << std::endl;
+        ///////////////////////////////////////////////////////////////
+
+    }
+    catch(const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
     return (0);
 }
